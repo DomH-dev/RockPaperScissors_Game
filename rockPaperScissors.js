42b5c1c5497aa96e7f8b5b59cd0839237ba976f4
Dom Hampshire <110022515+DomH-dev@users.noreply.github.com>
2024-04-13 14:43:46 +0100

    refactor: major refactor of js functions
    completion of getPlayerChoice function to use the
    UI along with fade in/out transition of icons.
    Initial preparations of other functions for use with
    DOM manipulation rather than console.logging

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,20 +1,9 @@
 /**
- * 
- * @param {string} string can be any string without special characters
- * @returns 
- */
-function inputCapitalise(string) {
-  string = string.toLowerCase();
-  string = string.charAt(0).toUpperCase() + string.slice(1);
-  return string;
-}
-
-/**
- * 
+ *
  * @returns a random choice of Rock, Paper or Scissors.
  */
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3 + 1); 
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
     if (randomNumber == 1) {
         return "Rock";
     } else if (randomNumber == 2) {
@@ -29,28 +18,68 @@ function getComputerChoice() {
  * @returns the players choice for use in the Rock, Paper Scissors game.
  */
 function getPlayerChoice() {
-    let validAnswer = 0; //
-    // loop below will keep prompting the user until the correct input is made.
-    while (validAnswer === 0) {
-        let playerInput = inputCapitalise(prompt("Please make a choice! Rock, Paper or Scissors?"));
-        if (
-            playerInput != "Rock" &&
-            playerInput != "Paper" &&
-            playerInput != "Scissors"
-        ) {
-            alert("Your input is invalid, please choose either Rock, Paper or Scissors.");
-            validAnswer = 0;
-        } else if (playerInput === "Rock") {
-            validAnswer = 1;
-            playerChoice = playerInput;
-        } else if (playerInput === "Paper") {
-            validAnswer = 1;
-            playerChoice = playerInput;
-        } else if (playerInput === "Scissors") {
-            validAnswer = 1;
-            playerChoice = playerInput;
-        }
-    }
+    const rockButton = document.querySelector("#rockButton");
+    const paperButton = document.querySelector("#paperButton");
+    const scissorButton = document.querySelector("#scissorButton");
+    const playerContainer = document.querySelector(".container_user");
+
+    rockButton.addEventListener("click", () => {
+        let playerChoice = "Rock";
+        paperButton.style.display = "flex";
+        scissorButton.style.display = "flex";
+        paperButton.classList.toggle("btn_hidden");
+        scissorButton.classList.toggle("btn_hidden");
+        paperButton.addEventListener("transitionend", function (e) {
+            if (e.propertyName === "opacity") {
+                paperButton.style.display = "none";
+            }
+        });
+        scissorButton.addEventListener("transitionend", function (e) {
+            if (e.propertyName === "opacity") {
+                scissorButton.style.display = "none";
+            }
+        });
+        return playerChoice;
+    });
+
+    paperButton.addEventListener("click", () => {
+        let playerChoice = "Paper";
+        rockButton.style.display = "flex";
+        scissorButton.style.display = "flex";
+        rockButton.classList.toggle("btn_hidden");
+        scissorButton.classList.toggle("btn_hidden");
+        rockButton.addEventListener("transitionend", function (e) {
+            if (e.propertyName === "opacity") {
+                rockButton.style.display = "none";
+            }
+        });
+        scissorButton.addEventListener("transitionend", function (e) {
+            if (e.propertyName === "opacity") {
+                scissorButton.style.display = "none";
+            }
+        });
+        return playerChoice;
+    });
+
+    scissorButton.addEventListener("click", () => {
+        let playerChoice = "Scissors";
+        rockButton.style.display = "flex";
+        paperButton.style.display = "flex";
+        rockButton.classList.toggle("btn_hidden");
+        paperButton.classList.toggle("btn_hidden");
+        rockButton.addEventListener("transitionend", function (e) {
+            if (e.propertyName === "opacity") {
+                rockButton.style.display = "none";
+            }
+        });
+        paperButton.addEventListener("transitionend", function (e) {
+            if (e.propertyName === "opacity") {
+                paperButton.style.display = "none";
+            }
+        });
+        return playerChoice;
+    });
+
     return playerChoice;
 }
 
@@ -58,68 +87,62 @@ function getPlayerChoice() {
  * This function is used to play a single round of Rock Paper Scissors.
  * @param {string} playerSelection this should call the function that prompts the user for an input
  * @param {string} computerSelection this should call the function to generate a random choice for the computer
- * @returns 
+ * @returns
  */
 function playRound(playerSelection, computerSelection) {
-
-  switch (true) {
-    case (playerSelection === computerSelection):
-      console.log("It's a draw!");
-      result = "draw";
-      break;
-    case (playerSelection === "Rock" && computerSelection === "Scissors"):
-    case (playerSelection === "Paper" && computerSelection === "Rock"):
-    case (playerSelection === "Scissors" && computerSelection === "Paper"):
-      console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-      result = "win";
-      break;
-    default:
-      console.log(`You lose this round! ${computerSelection} beats ${playerSelection}.`);
-      result = "lose";
-      break;
-  }
-  return result;
+    switch (true) {
+        case playerSelection === computerSelection:
+            console.log("It's a draw!");
+            result = "draw";
+            break;
+        case playerSelection === "Rock" && computerSelection === "Scissors":
+        case playerSelection === "Paper" && computerSelection === "Rock":
+        case playerSelection === "Scissors" && computerSelection === "Paper":
+            console.log(
+                `You win! ${playerSelection} beats ${computerSelection}.`
+            );
+            result = "win";
+            break;
+        default:
+            console.log(
+                `You lose this round! ${computerSelection} beats ${playerSelection}.`
+            );
+            result = "lose";
+            break;
+    }
+    return result;
 }
 
 function checkWinCondition(playerWins, computerWins) {
-  if (playerWins > computerWins) {
-    console.log(`You have won the game! Congratulations!`);
-  } else if (computerWins > playerWins) {
-    console.log(`You have lost the game! Sorry to hear that!`);
-  } else {
-    console.log(`It's a draw, please refresh the browser if you wish to try again.`)
-  }
+    if (playerWins === 5) {
+        console.log(`You have won the game! Congratulations!`);
+        return true;
+    } else if (computerWins === 5) {
+        console.log(`You have lost the game! Sorry to hear that!`);
+        return true;
+    }
+    return false;
 }
-
 
 /**
- * This function can be called to play a 5 round game of rock paper scissors.
+ * This function can be called to play a first to 5 wins game of rock paper scissors.
  */
 function playGame() {
+    let playerWins = 0;
+    let computerWins = 0;
+    let roundCounter = 1;
 
-  let playerWins = 0;
-  let computerWins = 0;
-  let roundCounter = 1;
-
-  while (roundCounter < 6) {
-
-    console.log(`Round ${roundCounter}!`)
+    const resultsTextBox = document.querySelector(".container_results");
     let result = playRound(getPlayerChoice(), getComputerChoice());
-
     if (result === "lose") {
-      computerWins++;
+        computerWins++;
     } else if (result === "win") {
-      playerWins++;
+        playerWins++;
     }
-
-    console.log(`You have won ${playerWins} rounds total. The computer has won ${computerWins} rounds total.`);
     roundCounter++;
-  }
-
-  checkWinCondition(playerWins, computerWins);
+    checkWinCondition(playerWins, computerWins);
 }
 
-//initialise the variables for use wihin the functions.
 let computerChoice = "";
 let playerChoice = "";
 let result = "";
